b97b119105dc914fbb70882c9a72f661554f7ca2
PortalMaster555 <portalmaster55@gmail.com>
2022-04-27 22:49:43 -0500

    Added Battery Percentage and Time Remaining Indicators

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,22 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+//Modify this file to change what commands output to your statusbar, and recompile  using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+
+	//the following removes all info from the upower command but XX%
+	{"Int. Bat: ", "upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage: | sed  's/percentage://g' | sed 's/ //g'", 15, 0},
+
+	{"Ext. Bat: ", "upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep percentage: | sed  's/percentage://g' | sed 's/ //g'", 15, 0},
+
+	//removes everything but X.Xh
+	//note that TLI may show nothing when not depleting.
+	{"TLI: ", "upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep 'time to empty' | sed  's/time to empty://g' | sed 's/ //g' | sed -r s'/ours//'g", 60, 0},
+
+
+	{"TLE: ", "upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep 'time to empty' | sed  's/time to empty://g' | sed 's/ //g' | sed -r s'/ours//'g", 60, 0},
+
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
